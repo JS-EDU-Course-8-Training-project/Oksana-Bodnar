ae3b556e72a25c015192a5b6190516483468d07d
Oksana Bodnar <oksana.bodnar@o-bodnar-1.local>
2022-04-15 14:53:01 +0300

    JEC8-65, JEC8-72 - home page, article template

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/services/getArticles.service';
+import { Articles } from 'src/shared/models/articles.model';
+import { Tags } from 'src/shared/models/tags.model';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [HttpService]
 })
+   
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
+  public articles!: Articles[];
+  public tags!: Tags[];
 
+  getArticles() {
+    this.httpService
+      .getAllArticles()
+      .subscribe(data => {
+      this.articles = data;
+      })
+  }
 
-  ngOnInit(): void {
+  getTags() {
+    this.httpService
+      .getTags()
+      .subscribe(data => {
+      this.tags = data;
+      })
+  }
+
+  ngOnInit() {
+    this.getArticles();
+    this.getTags();
   }
 
 }
